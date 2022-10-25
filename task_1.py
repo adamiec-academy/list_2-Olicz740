9b6f085ee3203bd0de5478e0505811e9b37b951d
Olicz740 <115731309+Olicz740@users.noreply.github.com>
2022-10-25 10:47:25 +0200

    Update task_1.py

--- task_1.py
+++ task_1.py
@@ -1,2 +1,18 @@
-def remove_parentheses(text):
-    return text
+text ="Ala (nie) ma kota"
+def sen(text):
+    result = ""
+    is_inside = False
+
+    for letter in text:
+        if letter == "(":
+            is_inside = True
+            result = result
+        elif letter == ")":
+            is_inside = False
+        elif not is_inside:
+            result += letter
+
+    return result
+
+
+print(sen(text))
