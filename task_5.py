1e0ba3e15c6a4d7656b6f6d6e093c5b7c08c9d5c
Olicz740 <115731309+Olicz740@users.noreply.github.com>
2022-11-06 21:23:28 +0100

    Update task_5.py

--- task_5.py
+++ task_5.py
@@ -1,6 +1,27 @@
 def cipher(text, shift):
-    return text
-
-
+    result =""
+    for letter in text:
+        if letter == " ":
+            result += letter
+        elif letter == "Z":
+            result += chr(ord("A") + shift - 1)
+        elif letter == "z":
+            result += chr(ord("a") + shift - 1)
+        else :
+            result += chr(ord(letter) + shift)
+    print(result)
 def decipher(text, shift):
-    return text
+    result = ""
+    for letter in text:
+        if letter == " ":
+            result += letter
+        elif letter == "A":
+            result += chr(ord("Z") - shift + 1)
+        elif letter == "a":
+            result += chr(ord("z") - shift + 1)
+        else:
+            result += chr(ord(letter) - shift)
+    print(result)
+
+cipher("Imperator", 1)
+decipher("Jnqfsbups", 1)
