70f0021d5e41dd04458930088ae8317d071699fd
Olicz740 <115731309+Olicz740@users.noreply.github.com>
2022-11-07 19:29:21 +0100

    Update task_3.py

--- task_3.py
+++ task_3.py
@@ -4,11 +4,11 @@ def is_prime(n):
     elif n==2:
         return True
     else :
-        for i in range(2,n + 1):
+        for i in range(2,n+1):
             if (n % i) == 0:
                 return False
-            else:
-                return True
+
+            return True
 
 def is_diabolic(n):
     if "666" in str(n):
