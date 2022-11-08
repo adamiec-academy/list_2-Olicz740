b12d0a115dc40a618d3b7809d3b8ff3a2b1b1d7a
Olicz740 <115731309+Olicz740@users.noreply.github.com>
2022-11-08 08:55:24 +0100

    Update task_3.py

--- task_3.py
+++ task_3.py
@@ -1,24 +1,24 @@
 def is_prime(n):
-    if n <= 1 :
-       return False
-    elif n==2:
-        return True
-    else :
-        for i in range(2,n+1):
-            if (n % i) == 0:
-                return False
-
-            return True
+    prime = True
+    if n <= 1:
+        prime = False
+    elif n == 2:
+        prime = True
+    for i in range(2, n + 1):
+        if n % i == 0:
+            prime = False
+    return prime
 
 def is_diabolic(n):
     if "666" in str(n):
         return True
     else:
         return False
-
-result =0
+result = 0
 for i in range(1,100001):
     if is_prime(i) is True and is_diabolic(str(i)) is True:
         result += 1
         print(i)
 print(f"ilosc {result}")
+
+
