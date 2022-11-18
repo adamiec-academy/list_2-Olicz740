cfa35e400eddb7b791f7c6d380fef77430455c22
Olicz740 <115731309+Olicz740@users.noreply.github.com>
2022-11-18 20:10:04 +0100

    Update task_5.py

--- task_5.py
+++ task_5.py
@@ -1,12 +1,14 @@
 def cipher(text, shift):
     result =""
+    shift = shift % 26
     for letter in text:
         if letter == " ":
             result += letter
-        elif ord(letter) + shift > 90 and ord(letter) < 97:
-            result += chr(65 - 90 + ord(letter) + shift)
-        elif ord(letter) + shift > 122 and ord(letter) > 97:
-            result += chr(97 - 122 + ord(letter) + shift)
+        elif letter.isupper():
+            if ord(letter) + shift > ord('Z'):
+                    result += chr(ord(letter) + shift - 26)
+            elif ord (letter) + shift > ord('z'):
+                result += chr(ord(letter) + shift - 26)
         else :
             result += chr(ord(letter) + shift)
     return result
@@ -15,14 +17,10 @@ def decipher(text, shift):
     for letter in text:
         if letter == " ":
             result += letter
-        elif ord(letter) - shift < 65 and ord(letter) < 97:
-            result += chr(90 - 65 + ord(letter) - shift)
-        elif ord(letter) - shift < 97 and ord(letter) > 97:
-            result += chr(122 - 97 + ord(letter) - shift)
+        elif ord(letter) - shift < ord('A') and ord(letter) < 97:
+            result += chr(ord(letter) - shift + 26)
+        elif ord(letter) - shift < ord('a') and ord(letter) > 97:
+            result += chr(ord(letter) - shift + 26)
         else:
             result += chr(ord(letter) - shift)
     return result
-
-
-print(cipher("Imperator", 1))
-print(decipher("Jnqfsbups", 1))
